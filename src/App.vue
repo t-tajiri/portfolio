2f7bd3e6b8cd2d095ec15988f4ae32d0452daad2
t-tajiri <swallow.ava@gmail.com>
2020-03-10 17:11:31 +0900

    add skillset

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Skillset />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Skillset from './components/Skillset.vue'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
+    Skillset
   }
 }
 </script>
---
+++ src/components/Skillset.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container mx-auto w-64">
+    <div class="grid grid-cols-3">
+      <span>Java</span>
+      <span>MySQL</span>
+      <span>JavaScript</span>
+      <span>Vue.js</span>
+      <span>React</span>
+      <span>AWS</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld'
+}
+</script>
